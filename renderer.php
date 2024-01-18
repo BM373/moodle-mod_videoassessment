767b6aa58de7c2455b459611ef0645b6125b30ba
Brendon M <82206382+BM373@users.noreply.github.com>
2024-01-18 14:20:53 +0900

    Delete renderer.php

--- renderer.php
+++
@@ -1,7 +0,0 @@
-<?php
-use \videoassess;
-
-defined('MOODLE_INTERNAL') || die();
-
-require_once $CFG->dirroot . '/mod/videoassessment/class/renderer/renderer.php';
-require_once $CFG->dirroot . '/mod/videoassessment/class/renderer/print_renderer.php';
