3f3b946e066e159adc99b732b6fef62c2816c469
AL Rachels <drachels@drachels.com>
2024-03-08 10:38:58 -0600

    Update version.php

--- version.php
+++ version.php
@@ -1,6 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 20230926001;
+$plugin->version   = 2024030800;
 $plugin->requires  = 2011112900;
 $plugin->component = 'mod_videoassessment';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0 (Build: 2024030800)'; // User-friendly version number.
+$plugin->supported = [40, 403];
