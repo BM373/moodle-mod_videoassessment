3eb68bb33f95c0d722f6d5bb21834e684803c6f0
AL Rachels <drachels@drachels.com>
2024-10-24 12:52:46 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -40,54 +40,54 @@ if ($ADMIN->fulltree) {
 
     require_once($CFG->dirroot.'/mod/videoassessment/lib.php');
 
-	$formats = array(
-		'.mp4'  => '.mp4  - H.264',
-		'.webm' => '.webm - WebM',
-		'.ogv'  => '.ogv  - Theora',
-		'.flv'  => '.flv  - Flash Video',
-		);
-	$settings->add(
-		new admin_setting_configselect('videoassessment_videoformat',
-			get_string('videoformat', 'videoassessment'),
-			get_string('videoformatdesc', 'videoassessment'),
-			key($formats), $formats)
-		);
+    $formats = array(
+        '.mp4'  => '.mp4  - H.264',
+        '.webm' => '.webm - WebM',
+        '.ogv'  => '.ogv  - Theora',
+        '.flv'  => '.flv  - Flash Video',
+        );
+    $settings->add(
+        new admin_setting_configselect('videoassessment_videoformat',
+            get_string('videoformat', 'videoassessment'),
+            get_string('videoformatdesc', 'videoassessment'),
+            key($formats), $formats)
+        );
 
-	if (!class_exists('admin_setting_configtext_ffmpegcommand')) {
-		class admin_setting_configtext_ffmpegcommand extends admin_setting_configtext
-		{
-			public function validate($data)
-			{
-				if (strpos($data, '{INPUT}') <= stripos($data, 'ffmpeg') ||
-					strpos($data, '{OUTPUT}') <= stripos($data, 'ffmpeg'))
-				{
-					return get_string('validateerror', 'admin');
-				}
-				return true;
-			}
-		}
-	}
-	$settings->add(
-		new admin_setting_configtext_ffmpegcommand('videoassessment_ffmpegcommand',
-			get_string('ffmpegcommand', 'videoassessment'),
-			get_string('ffmpegcommanddesc', 'videoassessment'),
-			'/usr/local/bin/ffmpeg -i {INPUT} {OUTPUT}', PARAM_RAW, 60)
-		);
+    if (!class_exists('admin_setting_configtext_ffmpegcommand')) {
+        class admin_setting_configtext_ffmpegcommand extends admin_setting_configtext
+        {
+            public function validate($data)
+            {
+                if (strpos($data, '{INPUT}') <= stripos($data, 'ffmpeg') ||
+                    strpos($data, '{OUTPUT}') <= stripos($data, 'ffmpeg'))
+                {
+                    return get_string('validateerror', 'admin');
+                }
+                return true;
+            }
+        }
+    }
+    $settings->add(
+        new admin_setting_configtext_ffmpegcommand('videoassessment_ffmpegcommand',
+            get_string('ffmpegcommand', 'videoassessment'),
+            get_string('ffmpegcommanddesc', 'videoassessment'),
+            '/usr/local/bin/ffmpeg -i {INPUT} {OUTPUT}', PARAM_RAW, 60)
+        );
 
-	$settings->add(
-		new admin_setting_configtext_ffmpegcommand('videoassessment_ffmpegthumbnailcommand',
-			get_string('ffmpegthumbnailcommand', 'videoassessment'),
-			get_string('ffmpegthumbnailcommanddesc', 'videoassessment'),
-			'/usr/local/bin/ffmpeg -i {INPUT} -vframes 1 -s 137x91 -ss 1 {OUTPUT}', PARAM_RAW, 60)
-		);
+    $settings->add(
+        new admin_setting_configtext_ffmpegcommand('videoassessment_ffmpegthumbnailcommand',
+            get_string('ffmpegthumbnailcommand', 'videoassessment'),
+            get_string('ffmpegthumbnailcommanddesc', 'videoassessment'),
+            '/usr/local/bin/ffmpeg -i {INPUT} -vframes 1 -s 137x91 -ss 1 {OUTPUT}', PARAM_RAW, 60)
+        );
 
-	$settings->add(
-		new admin_setting_configtext('videoassessment_mp4boxcommand',
-			get_string('mp4boxcommand', 'videoassessment'),
-			get_string('mp4boxcommanddesc', 'videoassessment'),
-			'/usr/local/bin/MP4Box', PARAM_RAW, 60)
-		);
-	
+    $settings->add(
+        new admin_setting_configtext('videoassessment_mp4boxcommand',
+            get_string('mp4boxcommand', 'videoassessment'),
+            get_string('mp4boxcommanddesc', 'videoassessment'),
+            '/usr/local/bin/MP4Box', PARAM_RAW, 60)
+        );
+    
     $settings->add(new admin_setting_heading('backupdefaults', new lang_string('backupdefaults', 'videoassessment'), ''));
     $settings->add(
         new admin_setting_configcheckbox('videoassessment/backupusers',
