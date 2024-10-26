9bda33d3943ef8bdefaec50c184ef35735133d92
AL Rachels <drachels@drachels.com>
2024-10-26 12:12:36 -0500

    Update assess.php

--- class/form/assess.php
+++ class/form/assess.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for advance grading of videos for the videoassessment module.
+ *
+ * @package    mod_videoassessment
+ * @copyright  2024 Don Hinkleman (hinkelman@mac.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
 namespace videoassess\form;
 use \videoassess\va;
 
@@ -6,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 
 class assess extends \moodleform {
     /**
-     * @var stdClass stores the advaned grading instance (if used in grading)
+     * @var stdClass stores the advanced grading instance (if used in grading)
      */
     private $advancegradinginstance;
 
